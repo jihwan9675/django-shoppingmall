88376dfc3adf25e677d6e5b3d622fc433209b78e
Shinjihwan <jihwan9675>
2021-03-19 18:11:32 +0900

    로그인 기능 추가 1. fcuser/templates/login.html 작성 2. fcuser/views.py에 LoginView 클래스 추가, index 메소드에 session 인자 값 추가 3. fcuser/forms.py에 LoginForm 클래스 추가

--- fc_django/fcuser/forms.py
+++ fc_django/fcuser/forms.py
@@ -1,6 +1,31 @@
 from django import forms
 from .models import Fcuser
+from django.contrib.auth.hashers import check_password, make_password
 
+class LoginForm(forms.Form):
+    email = forms.EmailField(error_messages={
+        'required': '이메일을 입력해주세요.'
+    }, max_length=64,label="이메일")
+    password = forms.CharField(error_messages={
+        'required':'비밀번호를 입력해주세요.'
+    },widget=forms.PasswordInput, label="비밀번호")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                fcuser = Fcuser.objects.get(email=email)
+            except Fcuser.DoesNotExist:
+                self.add_error('email', ' 등록된 이메일이 없습니다.')
+                return
+            
+            if not check_password(password, fcuser.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
+            else:
+                self.email=fcuser.email
 
 class RegisterForm(forms.Form):
     email = forms.EmailField(
@@ -35,6 +60,6 @@ class RegisterForm(forms.Form):
             else:
                 fcuser = Fcuser(
                     email = email,
-                    password = password
+                    password = make_password(password)
                 )
                 fcuser.save()
