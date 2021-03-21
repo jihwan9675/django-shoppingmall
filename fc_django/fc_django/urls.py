59eaa062ae7884d514b0f1658277abd0bdceb758
Shinjihwan <jihwan9675>
2021-03-21 18:25:23 +0900

    상품 주문하기 기능 추가 1. order/forms.py 에 OrderCreate 클래스 작성 2. prodcut/views.py 수정 3. product/templates/[produict_detail.html 수정

--- fc_django/fc_django/urls.py
+++ fc_django/fc_django/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from fcuser.views import index, RegisterView, LoginView
 from product.views import ProductList, ProductCreate, ProductDetail
+from order.views import OrderCreate
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
@@ -24,5 +26,6 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('product/', ProductList.as_view()),
     path('product/create/', ProductCreate.as_view()),
-    path('product/<int:pk>', ProductDetail.as_view()),
+    path('product/<int:pk>/', ProductDetail.as_view()),
+    path('order/create/', OrderCreate.as_view()),
 ]
